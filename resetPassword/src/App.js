3ac3541dc39a8806801ab9917737eac3855b4e77
Ata <ataguneser@hotmail.com>
2022-09-25 22:07:45 +0300

    resetPassword

---
+++ resetPassword/src/App.js
@@ -0,0 +1,12 @@
+import ResPassword from './components/ResPassword';
+import './ResPassStyle.css'
+
+function App() {
+  return (
+    <div>
+      <ResPassword></ResPassword>
+    </div>
+  );
+}
+
+export default App;
