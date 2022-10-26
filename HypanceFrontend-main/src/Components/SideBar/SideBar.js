f3c3d69f67c968cd0b453e5215d145aef69786c6
Ata <ataguneser@hotmail.com>
2022-10-26 19:59:26 +0300

    SidBar css updated & close button added

--- HypanceFrontend-main/src/Components/SideBar/SideBar.js
+++ HypanceFrontend-main/src/Components/SideBar/SideBar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import * as BS from "react-bootstrap";
-import brokenicon from "../../images/Vector.png"
-import "./SideBar.css"
+import brokenicon from "../../images/Vector.png";
+import "./SideBar.css";
 
 function SideBar() {
   const [isActive, setIsActive] = useState(false)
