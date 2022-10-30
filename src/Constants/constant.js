6178f93250bfde134d1a52dc85fd146eda042f41
Süha Kesikbaş <suha.kesikbas@outlook.com>
2022-10-31 00:28:09 +0300

    Updated Constant

--- src/Constants/constant.js
+++ src/Constants/constant.js
@@ -1,5 +1,19 @@
-const baseURL =(address) => "http://localhost:31377/"+address;
-
+const baseUrl = "https://api.hypance.com/";
+const getSymbolUrl = baseUrl+"Symbol/GetAll";
+const getMarketUrl = baseUrl+"Enums/GetMarkets";
+const getOrderTypeUrl = baseUrl+"Enums/GetOrderTypes";
+const getPositionTypeUrl = baseUrl+"Enums/GetPositionTypes";
+const getStrategiesUrl = baseUrl+"Strategy/GetAll";
+const getTrendStrategies = baseUrl+"Enums/GetTrendStrategies";
+const createStrategyUrl = baseUrl+"Symbol/GetAll";
+const createBotUrl = baseUrl+"Bot/Post";
 module.exports={
-    baseURL
+    getSymbolUrl,
+    getMarketUrl,
+    getOrderTypeUrl,
+    getPositionTypeUrl,
+    getStrategiesUrl,
+    getTrendStrategies,
+    createStrategyUrl,
+    createBotUrl
 }
