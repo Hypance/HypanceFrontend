08409352a66a8cf10c7becfdaa0dc4484f85e302
ezgikarali <ezgi.karali505@gmail.com>
2022-11-04 19:23:32 +0300

    bg changed

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,59 +1,19 @@
 import React, { useState } from 'react';
-import {
-  MDBTabs,
-  MDBTabsItem,
-  MDBTabsLink,
-  MDBTabsContent,
-  MDBTabsPane,
-  MDBFile,
-  MDBInput,
-  MDBBtn,
-} from "mdb-react-ui-kit";
 import anonim from "./anonim.jpg";
 import * as BS from "react-bootstrap";
 
+
 export function Settings() {
-   const [fillActive, setFillActive] = useState("tab1");
 
-   const handleFillClick = (value) => {
-     if (value === fillActive) {
-       return;
-     }
-
-     setFillActive(value);
-   };
 
    return (
      <>
-       <MDBTabs fill className="mb-3">
-         <MDBTabsItem>
-           <MDBTabsLink
-             onClick={() => handleFillClick("tab1")}
-             active={fillActive === "tab1"}
-           >
-             Profile
-           </MDBTabsLink>
-         </MDBTabsItem>
-         <MDBTabsItem>
-           <MDBTabsLink
-             onClick={() => handleFillClick("tab2")}
-             active={fillActive === "tab2"}
-           >
-             Notifications
-           </MDBTabsLink>
-         </MDBTabsItem>
-         <MDBTabsItem>
-           <MDBTabsLink
-             onClick={() => handleFillClick("tab3")}
-             active={fillActive === "tab3"}
-           >
-             Security
-           </MDBTabsLink>
-         </MDBTabsItem>
-       </MDBTabs>
-
-       <MDBTabsContent>
-         <MDBTabsPane show={fillActive === "tab1"}>
+       <BS.Tabs fill className="mb-3">
+         <BS.Tab
+           className="mb-3 bg-white text-black"
+           eventKey="first"
+           title="Profile"
+         >
            <BS.Container>
              <BS.Row>
                <BS.Col>
@@ -66,90 +26,57 @@ export function Settings() {
                      width="100"
                      height="100"
                    />
-
-                   <MDBFile className="" id="customFile" />
                  </div>
                  <div>
-                   <form>
-                     <MDBInput
-                       id="form4Example1"
-                       wrapperClass="mb-4"
-                       label="Name"
-                     />
-                     <MDBInput
-                       type="email"
-                       id="form4Example2"
-                       wrapperClass="mb-4"
-                       label="Email address"
-                     />
-                     <MDBInput
-                       wrapperClass="mb-4"
-                       textarea
-                       id="form6Example7"
-                       rows={4}
-                       label="Additional information"
-                     />
+                   <BS.Form>
+                     <BS.Form.Group className="mb-3" controlId="formBasicEmail">
+                       <BS.Form.Label>Email address</BS.Form.Label>
+                       <BS.Form.Control
+                         type="email"
+                         placeholder="Enter email"
+                       />
+                       <BS.Form.Text className="text-muted">
+                         We'll never share your email with anyone else.
+                       </BS.Form.Text>
+                     </BS.Form.Group>
 
-                     <BS.Button variant="primary" size="lg">
+                     <BS.Form.Group
+                       className="mb-3"
+                       controlId="exampleForm.ControlTextarea1"
+                     >
+                       <BS.Form.Label>Example textarea</BS.Form.Label>
+                       <BS.Form.Control as="textarea" rows={3} />
+                     </BS.Form.Group>
+
+                     <BS.Button variant="primary" size="lg" type="submit">
                        Block level button
                      </BS.Button>
-                   </form>
+                   </BS.Form>
                  </div>
                </BS.Col>
                <BS.Col></BS.Col>
              </BS.Row>
            </BS.Container>
-         </MDBTabsPane>
-         <MDBTabsPane show={fillActive === "tab2"}>
+         </BS.Tab>
+         <BS.Tab
+           className="mb-3 bg-white text-black"
+           eventKey="second"
+           title="Notifications"
+         >
            <BS.Container>
              <BS.Row>
-               <BS.Col>
-                 <h5>GENERAL INFORMATION</h5>
-                 <div className="d-flex ">
-                   <img
-                     alt=""
-                     className="rounded-circle"
-                     src={anonim}
-                     width="100"
-                     height="100"
-                   />
-
-                   <MDBFile className="" id="customFile" />
-                 </div>
-                 <div>
-                   <form>
-                     <MDBInput
-                       id="form4Example1"
-                       wrapperClass="mb-4"
-                       label="Name"
-                     />
-                     <MDBInput
-                       type="email"
-                       id="form4Example2"
-                       wrapperClass="mb-4"
-                       label="Email address"
-                     />
-                     <MDBInput
-                       wrapperClass="mb-4"
-                       textarea
-                       id="form6Example7"
-                       rows={4}
-                       label="Additional information"
-                     />
-
-                     <MDBBtn type="submit" className="mb-4" block>
-                       Sign in
-                     </MDBBtn>
-                   </form>
-                 </div>
-               </BS.Col>
+               <BS.Col></BS.Col>
                <BS.Col></BS.Col>
              </BS.Row>
            </BS.Container>
-         </MDBTabsPane>
-         <MDBTabsPane show={fillActive === "tab3"}>
+         </BS.Tab>
+         <BS.Tab
+           className="mb-3 bg-white text-black"
+           eventKey="third"
+           title="Security"
+         >
            <BS.Container>
-             <BS.Row>
+             <BS.Row className="mt-5">
                <BS.Col>
                  <h5>PASSWORD</h5>
                  <p>Change your password to a new one.</p>
@@ -161,7 +88,7 @@ export function Settings() {
                  </BS.Button>{" "}
                </BS.Col>
              </BS.Row>
-             <BS.Row>
+             <BS.Row className="mt-5">
                <BS.Col>
                  <h5>MULTI-FACTOR AUTHENTICATION</h5>
                  <p>Setup or remove MFA devices.</p>
@@ -174,10 +101,9 @@ export function Settings() {
                </BS.Col>
              </BS.Row>
            </BS.Container>
-         </MDBTabsPane>
-       </MDBTabsContent>
+         </BS.Tab>
+       </BS.Tabs>
      </>
-   );
-}
+   );}
 
 export default Settings;
