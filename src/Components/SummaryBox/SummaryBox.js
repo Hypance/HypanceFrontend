7a112ef5cb6116a67ab3832f0138f72831098486
ezgikarali <ezgi.karali505@gmail.com>
2022-11-04 17:00:56 +0300

    button activated

--- src/Components/SummaryBox/SummaryBox.js
+++ src/Components/SummaryBox/SummaryBox.js
@@ -1,38 +1,50 @@
-import Button from "react-bootstrap/Button";
-
+import * as BS from "react-bootstrap";
 import React from "react";
+import {Routes,Route, useNavigate} from 'react-router-dom';
 
-const SummaryBox = () => {
+
+
+function SummaryBox() {
+  const navigate = useNavigate();
+
+  const navigateToCreateStrategies = () => {
+    navigate("/strategy");
+  };
+
   return (
-    <div class="grid grid-cols-6 gap-4">
-      <div class="col-start-2 col-span-4 ..."></div>
-      <div class="col-end-7 col-span-2 ...">
-        <div class="flex justify-center">
-          <div class=" bg-white max-w-sm">
-            <div class="p-6">
-              <Button variant="primary">Back to Strategies</Button>{" "}
-              <h1
-                class="text-gray-900 text-xl mb-2 mt-14 font-bold"
-                style={{ fontFamily: "Space Grotesk" }}
-              >
-                Create Strategy Wizard
-              </h1>
-              <h5
-                class="text-gray-900 text-md mt-5  mb-2 font-bold "
-                style={{ fontFamily: "Space Grotesk" }}
-              >
-                Summary
-              </h5>
-              <p class="text-gray-500 text-base mb-4">
-                Enter the 4-digit code that we have sent to your phone number,
-                +62 8231234231
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <BS.Container>
+        <BS.Row>
+          <BS.Button
+            onClick={navigateToCreateStrategies}
+            variant="primary"
+            size="lg"
+            active
+          >
+            Back to Strategies
+          </BS.Button>
+        </BS.Row>
+        <BS.Row>
+          <h5 className="mt-5" style={{ fontFamily: "Space Grotesk" }}>
+            Create Strategy Wizard
+          </h5>
+        </BS.Row>
+        <BS.Row>
+          
+          <h5
+            className="text-gray-900 mt-5 mb-2 "
+            style={{ fontFamily: "Space Grotesk" }}
+          >
+            Summary
+          </h5>
+          <p className="text-gray-500 text-base mb-4">
+            Enter the 4-digit code that we have sent to your phone number, +62
+            8231234231
+          </p>
+        </BS.Row>
+      </BS.Container>
+    </>
   );
-};
+}
 
 export default SummaryBox;
