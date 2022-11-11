85e17213a98870eaa079d1800ad61a060dac9979
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2022-11-11 14:39:15 +0300

    Updated TopBar

--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -5,7 +5,7 @@ import { NavDropdown, Form } from "react-bootstrap";
 import dollar from "./dollar.png";
 // import btc from "./btc.png";
 import avatar from "./avatar.jpg";
-import zil from "./bell.jpg";
+import bell from "./bell.jpg";
 function TopBar() {
   return (
     <div className='topBar'>
@@ -16,9 +16,7 @@ function TopBar() {
           </Form>{" "}
         </div>
       </div>
-
       <div>
-        <span className="space2"></span>
         <span className="Cryptos">Cryptos:</span>
         <span className="CryptosData">27,200</span>
         <span className="space"></span>
@@ -28,38 +26,36 @@ function TopBar() {
         <span className="Cryptos">Dominance:</span>
         <span className="CryptosData">BTC 39.7%</span>
       </div>
-      <div className="mt-8">
-        <div className="box">
-          <img alt="" src={dollar} width="18" height="18" />
-          {"USD"}
-          <NavDropdown id="nav-dropdown-dark-example" title="" menuVariant="white">
-            <NavDropdown.Item href="#action/3.1">
-              <img alt="" src={dollar} width="18" height="18" />
-              {"TRY"}
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              <img alt="" src={dollar} width="18" height="18" />
-              {"BTC"}
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">
-              <img alt="" src={dollar} width="18" height="18" />
-              {"ETH"}
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Choose a currency</NavDropdown.Item>
-          </NavDropdown>
-        </div>
-        </div>
-        <div className="ntfc">
-          <div className="bell">
-            <img alt="bell" src={zil} width="18" height="18" />
-          </div>
-        </div>
-        <div className="Avatar">
-          <img alt="avatar" src={avatar} width="44" height="44" />
+      <div className="box">
+        <img alt="" src={dollar} width="18" height="18" />
+        {"USD"}
+        <NavDropdown id="nav-dropdown-dark-example" title="" menuVariant="white">
+          <NavDropdown.Item>
+            <img alt="" src={dollar} width="18" height="18" />
+            {"TRY"}
+          </NavDropdown.Item>
+          <NavDropdown.Item>
+            <img alt="" src={dollar} width="18" height="18" />
+            {"BTC"}
+          </NavDropdown.Item>
+          <NavDropdown.Item>
+            <img alt="" src={dollar} width="18" height="18" />
+            {"ETH"}
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item>Choose a currency</NavDropdown.Item>
+        </NavDropdown>
+      </div>
+      <div className="ntfc">
+        <div className="bell">
+          <img alt="bell" src={bell} width="18" height="18" />
         </div>
       </div>
-      );
+      <div className="space">
+        <img alt="avatar" src={avatar} width="44" height="44" />
+      </div>
+    </div>
+  );
 }
 
-      export default TopBar;
+export default TopBar;
